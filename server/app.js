a1286185424423a19a488a67ee506c3c8bb89a54
TTumyr <trond.tumyr@online.no>
2019-10-13 16:43:42 +0200

    added basic server setup with mongoDB connection

--- server/app.js
+++ server/app.js
@@ -1,4 +1,40 @@
+// Basic server components
 const express = require("express");
+const session = require("express-session");
+
+// Server configured components
+const mdb = require("./utils/mdb");
+const system = require("./utils/config").system();
+const mdbStore = mdb.store(system);
+
+// Express init
 const app = express();
 
-app.listen(3000, console.log("server started on 3000"));
+// MongoDB connection
+const mdbInit = async () => {
+  await mdb.connect(app, system);
+};
+
+// Setup used components in app
+const appInit = async () => {
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+  app.use(
+    session({
+      secret: "1234#changeOnDeploy",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+      },
+      store: mdbStore
+    })
+  );
+};
+
+//Start server
+const serverStart = async () => {
+  await mdbInit();
+  appInit();
+};
+serverStart();

09a05e597abae784e02f0233d189d734b0c1f3d5
TTumyr <trond.tumyr@online.no>
2019-11-05 17:34:23 +0100

    react naming conventions

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,30 +1,41 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React, { Component } from "./node_modules/react";
+import "./node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./node_modules/bootstrap/dist/js/bootstrap.bundle.min";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebookSquare, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "./node_modules/@fortawesome/react-fontawesome";
+import {
+  faFacebookSquare,
+  faTwitter,
+  faInstagram
+} from "./node_modules/@fortawesome/free-brands-svg-icons";
 
-import './Footer.css'
+import "./Footer.css";
 
 class Footer extends Component {
-  render(){
+  render() {
     return (
       <div className="container">
         <div className="card text-center bg-dark">
           <div className="card-footer text-light">
-            Copyright © 2019 Flight Booking App. <br/>
-            Flight Booking App is the world leader in online travel & related services.
+            Copyright © 2019 Flight Booking App. <br />
+            Flight Booking App is the world leader in online travel & related
+            services.
           </div>
           <hr className="bg-light w-75 text-center mx-auto" />
           <div className="pb-2">
-            <a href="#" className="fa fa-facebook"><FontAwesomeIcon icon={faFacebookSquare} /></a>
-            <a href="#" className="fa fa-twitter"><FontAwesomeIcon icon={faTwitter} /></a>
-            <a href="#" className="fa fa-instagram"><FontAwesomeIcon icon={faInstagram} /></a>
+            <a href="#" className="fa fa-facebook">
+              <FontAwesomeIcon icon={faFacebookSquare} />
+            </a>
+            <a href="#" className="fa fa-twitter">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+            <a href="#" className="fa fa-instagram">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import React from "./node_modules/react";
+import { FontAwesomeIcon } from "./node_modules/@fortawesome/react-fontawesome";
+import { faSearch } from "./node_modules/@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
   return (

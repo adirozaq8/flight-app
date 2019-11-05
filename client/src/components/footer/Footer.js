000943287c16beabab8315d2231d8a746f2cd817
Rozaq Adi <reza.gerrard@gmail.com>
2019-11-05 23:17:50 +0700

    adding social media icon to the footer

---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookSquare, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
+
+import './Footer.css'
+
+class Footer extends Component {
+  render(){
+    return (
+      <div className="container">
+        <div className="card text-center bg-dark">
+          <div className="card-footer text-light">
+            Copyright © 2019 Flight Booking App. <br/>
+            Flight Booking App is the world leader in online travel & related services.
+          </div>
+          <hr className="bg-light w-75 text-center mx-auto" />
+          <div className="pb-2">
+            <a href="#" className="fa fa-facebook"><FontAwesomeIcon icon={faFacebookSquare} /></a>
+            <a href="#" className="fa fa-twitter"><FontAwesomeIcon icon={faTwitter} /></a>
+            <a href="#" className="fa fa-instagram"><FontAwesomeIcon icon={faInstagram} /></a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Footer;

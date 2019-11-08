516a9820d53b9a072cd2ff350c1302c26712695f
TTumyr <trond.tumyr@online.no>
2019-11-08 20:37:53 +0100

    added Skyscanner widget

--- client/src/components/jumbotron/Jumbotron.jsx
+++ client/src/components/jumbotron/Jumbotron.jsx
@@ -1,72 +1,14 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import Skyload from "../skyload/Skyload";
 import "./Jumbotron.css";
 
 function Jumbotron() {
   return (
     <div>
-      <div class="container">
-        <form action="" className="form-inline">
-          <div className="container">
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Origin"
-                aria-label="Origin"
-              />
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Destination"
-                aria-label="Destination"
-              />
-              <div className="input-group-append">
-                <span className="input-group-text" id="jumbo_search">
-                  <div className="MainFrame__fa-icon">
-                    <FontAwesomeIcon icon={faSearch} />
-                  </div>
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="container">
-            <div className="input-group">
-              <input
-                type="date"
-                className="form-control"
-                placeholder="Calendar date departure"
-                defaultValue={Date.now()}
-              />
-              <input
-                type="date"
-                className="form-control"
-                placeholder="Calendar date arrival"
-                defaultValue={Date.now()}
-              />
-            </div>
-          </div>
-          <div className="container">
-            <div className="input-group">
-              <input
-                type="number"
-                className="form-control"
-                placeholder="Number of passengers"
-                defaultValue="1"
-              />
-              <select name="cars">
-                <option value="select" selected disabled>
-                  {"< select class >"}
-                </option>
-                <option value="first">First class</option>
-                <option value="business">Business</option>
-                <option value="premium">Premium</option>
-                <option value="economy">Economy</option>
-              </select>
-            </div>
-          </div>
-        </form>
+      <div className="Skyload-wrapper">
+        <Skyload />
       </div>
     </div>
   );
---
+++ client/src/components/skyload/Skyload.jsx
@@ -0,0 +1,28 @@
+import React, { Component, useEffect } from "react";
+import axios from "axios";
+import "./Skyload.css";
+
+const Skyload = () => {
+  useEffect(() => {
+    const anchor = document.createElement("div");
+    anchor.setAttribute("id", "SkySearch");
+    anchor.setAttribute("data-skyscanner-widget", "SearchWidget");
+    document.getElementById("skySearch").appendChild(anchor);
+
+    const script = document.createElement("script");
+    script.setAttribute(
+      "src",
+      "https://widgets.skyscanner.net/widget-server/js/loader.js"
+    );
+    document.getElementById("skySearch").appendChild(script);
+  }, []);
+  return (
+    <div>
+      <div className="container skyContainer">
+        <div id="skySearch"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Skyload;

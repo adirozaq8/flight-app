30dcb8feb8f9c9744406843da4047b08774b33d2
TTumyr <trond.tumyr@online.no>
2020-01-06 23:13:56 +0100

    structure additions

--- admin/src/pages/Admin/Admin.jsx
+++ admin/src/pages/Admin/Admin.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Data from "../../data/Data";
 import UserPanel from "./components/UserPanel";
+import "./Admin.css";
 
 function Admin() {
   return (
-    <div>
-      <UserPanel Users={Data.Users} />
+    <div className="Admin">
+      <div className="Admin__UserPanel">
+        <UserPanel Users={Data.Users} />
+      </div>
     </div>
   );
 }

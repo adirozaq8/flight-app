3fdf369361c50dd3d2c06995de3592e40c75bc06
TTumyr <trond.tumyr@online.no>
2020-04-24 23:20:21 +0200

    added basic flight search html form

---
+++ client/src/components/amadeus/AmForm.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import "./AmForm.css";
+
+class AmForm extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <form className="AmForm">
+        <div className="form__radio-group">
+          <input
+            type="radio"
+            id="AmOneWay"
+            name="flight-route"
+            value="oneWay"
+          />
+          <label htmlFor="AmOneWay">One way</label>
+          <input
+            type="radio"
+            id="AmTypeReturn"
+            name="flight-route"
+            value="typeReturn"
+          />
+          <label htmlFor="AmTypeReturn">Return</label>
+          <input type="radio" id="AmMulti" name="flight-route" value="multi" />
+          <label htmlFor="AmMulti">Multi-city</label>
+        </div>
+        <div className="form__input-group">
+          <label htmlFor="AmFrom">From</label>
+          <input type="text" id="AmFrom" name="from" />
+        </div>
+        <div className="form__input-group">
+          <label htmlFor="AmTo">To</label>
+          <input type="text" id="AmTo" name="to" />
+        </div>
+        <div className="form__input-group">
+          <label htmlFor="AmDepart">Departure</label>
+          <input type="text" id="AmDepart" name="depart" />
+        </div>
+        <div className="form__input-group">
+          <label htmlFor="AmReturn">Return</label>
+          <input type="text" id="AmReturn" name="return" />
+        </div>
+        <div className="form__input-group">
+          <label htmlFor="AmClass">Cabin class</label>
+          <select id="AmClass">
+            <option value="volvo">Economy</option>
+            <option value="volvo">Premium economy</option>
+            <option value="volvo">Business</option>
+            <option value="volvo">First class</option>
+          </select>
+        </div>
+        <div className="form__input-group">
+          <label htmlFor="AmAdults">Adults</label>
+          <input type="number" id="AmAdults" name="adults" />
+          <label htmlFor="AmChildren">Children</label>
+          <input type="number" id="AmChildren" name="children" />
+        </div>
+        <button id="AmSubmit" type="submit">
+          Find flights
+        </button>
+      </form>
+    );
+  }
+}
+
+export default AmForm;

53a880b1c76af5cdcaf1a6a604b1ad558af649c8
Rozaq Adi <reza.gerrard@gmail.com>
2020-04-27 17:22:35 +0700

    add validators for user

--- server/models/User.js
+++ server/models/User.js
@@ -1,21 +1,37 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt");
-// const validator = require("validator");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    validate: {
+      validator: function(v){
+        return validator.isEmail(v)
+      },
+      message: props => `${props.value} is not a valid email!`
+    }
   },
   username: {
     type: String,
-    required: true,
-    unique: true
+    required: [true, "You must provide a username."],
+    unique: true, 
+    validate: {
+      validator: function(v){
+        return validator.isAlphanumeric(v)
+      },
+      message: () => `Username can only contain letters and numbers.!`
+    },
+    minlength: [3, "Username must be at least 3 characters."],
+    maxlength: [30, "Username cannot exceed 30 characters."]
   },
   password: {
     type: String,
-    required: true,
+    required: [true, "You must provide a password."],
+    minlength: [8, "Password must be at least 8 characters."],
+    maxlength: [50, "Password cannot exceed 50 characters."]
   },
   avatar: {
     type: String
@@ -46,36 +62,6 @@ userSchema.methods.comparePassword = async function(candidatePassword, next){
 
 // User.prototype.validate = function() {
 //   return new Promise(async (resolve, reject) => {
-//     if (this.body.username == "") {
-//       this.errors.push("You must provide a username.");
-//     }
-//     if (
-//       this.body.username != "" &&
-//       !validator.isAlphanumeric(this.body.username)
-//     ) {
-//       this.errors.push("Username can only contain letters and numbers.");
-//     }
-//     if (!validator.isEmail(this.body.email)) {
-//       this.errors.push("You must provide a valid email address.");
-//     }
-//     if (this.body.password !== this.body.passwordR) {
-//       this.errors.push("Passwords do not match.");
-//     }
-//     if (this.body.password == "") {
-//       this.errors.push("You must provide a password.");
-//     }
-//     if (this.body.password.length > 0 && this.body.password.length < 3) {
-//       this.errors.push("Password must be at least 12 characters.");
-//     }
-//     if (this.body.password.length > 50) {
-//       this.errors.push("Password cannot exceed 50 characters.");
-//     }
-//     if (this.body.username.length > 0 && this.body.username.length < 3) {
-//       this.errors.push("Username must be at least 3 characters.");
-//     }
-//     if (this.body.username.length > 30) {
-//       this.errors.push("Username cannot exceed 30 characters.");
-//     }
 //     if (
 //       this.body.username.length > 2 &&
 //       this.body.username.length < 31 &&
@@ -84,9 +70,6 @@ userSchema.methods.comparePassword = async function(candidatePassword, next){
 //       let usernameExists = await mDb.findOne({
 //         username: this.body.username
 //       });
-//       if (usernameExists) {
-//         this.errors.push("That username is already taken.");
-//       }
 //     }
 //     if (validator.isEmail(this.body.email)) {
 //       let emailExists;
@@ -95,9 +78,6 @@ userSchema.methods.comparePassword = async function(candidatePassword, next){
 //         emailExists = await mDb.findOne({
 //           email: this.body.email
 //         });
-//       }
-//       if (emailExists) {
-//         this.errors.push("That email is already being used.");
 //       }
 //     }
 //     resolve();

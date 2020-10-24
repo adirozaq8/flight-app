e163aea0181dd196df76ee9518c1b7120ce19006
TTumyr <trond.tumyr@online.no>
2020-10-24 19:11:59 +0200

    minor

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 import "./navbar.css";
-import classes from './navbar.module.css';
+import classes from "./navbar.module.css";
 
 class Navbar extends React.Component {
   //
@@ -28,7 +28,7 @@ class Navbar extends React.Component {
           >
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-              <SearchBar />
+                <SearchBar />
               </li>
               <li className="nav-item">
                 <button
@@ -60,8 +60,8 @@ class Navbar extends React.Component {
   }
 }
 
-function SearchBar(){
-  return(
+function SearchBar() {
+  return (
     <form action="" className="form-inline">
       <div className="input-group">
         <input
@@ -71,7 +71,7 @@ function SearchBar(){
           aria-label="Search"
           aria-describedby="basic-addon1"
         />
-        <div className={['input-group-append', classes.searchButton].join(' ')}>
+        <div className={["input-group-append", classes.searchButton].join(" ")}>
           <div className="input-group-text" id="basic-addon1">
             <div className="MainFrame__fa-icon">
               <FontAwesomeIcon icon={faSearch} />
@@ -80,142 +80,142 @@ function SearchBar(){
         </div>
       </div>
     </form>
-  )
+  );
 }
 
-function LoginModal(){
-  return(
-          <div
-            className="modal fade"
-            id="loginModal"
-            tabIndex="-1"
-            role="dialog"
-            aria-labelledby="exampleModalLabel"
-            aria-hidden="true"
-          >
-            <div className="modal-dialog" role="document">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title" id="exampleModalLabel">
-                    Log In
-                  </h5>
-                  <button
-                    type="button"
-                    className="close"
-                    data-dismiss="modal"
-                    aria-label="Close"
-                  >
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div className="modal-body">
-                  <form action="/login" method="POST">
-                    <div className="form-group">
-                      <label htmlFor="usernameLogin">Username</label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="usernameLogin"
-                        placeholder="Enter username"
-                      />
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="passwordLogin">Password</label>
-                      <input
-                        type="password"
-                        className="form-control"
-                        id="passwordLogin"
-                        placeholder="Enter password"
-                      />
-                    </div>
-                    <div className="modal-footer">
-                      <button type="submit" className="btn btn-primary">
-                        Login
-                      </button>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
+function LoginModal() {
+  return (
+    <div
+      className="modal fade"
+      id="loginModal"
+      tabIndex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              Log In
+            </h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
           </div>
-  )
-}
-
-function SignupModal(){
-  return(
-          <div
-          className="modal fade"
-          id="signupModal"
-          tabIndex="-1"
-          role="dialog"
-          aria-labelledby="exampleModalLabel"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">
-                  Sign Up
-                </h5>
-                <button
-                  type="button"
-                  className="close"
-                  data-dismiss="modal"
-                  aria-label="Close"
-                >
-                  <span aria-hidden="true">&times;</span>
+          <div className="modal-body">
+            <form action="/login" method="POST">
+              <div className="form-group">
+                <label htmlFor="usernameLogin">Username</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="usernameLogin"
+                  placeholder="Enter username"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="passwordLogin">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="passwordLogin"
+                  placeholder="Enter password"
+                />
+              </div>
+              <div className="modal-footer">
+                <button type="submit" className="btn btn-primary">
+                  Login
                 </button>
               </div>
-              <div className="modal-body">
-                <form action="/register" method="POST">
-                  <div className="form-group">
-                    <label htmlFor="usernameSignup">Username</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="usernameSignup"
-                      placeholder="Enter username"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="passwordSignup">Password</label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="passwordSignup"
-                      placeholder="Enter password"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="emailSignup">Email address</label>
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="emailSignup"
-                      aria-describedby="emailHelp"
-                      placeholder="Enter email address"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="phoneNumberSignup">Phone Number</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="phoneNumberSignup"
-                      placeholder="Enter phone number"
-                    />
-                  </div>
-                  <div className="modal-footer">
-                    <button type="submit" className="btn btn-primary">
-                      Sign Up
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
+            </form>
           </div>
         </div>
-  )
+      </div>
+    </div>
+  );
+}
+
+function SignupModal() {
+  return (
+    <div
+      className="modal fade"
+      id="signupModal"
+      tabIndex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              Sign Up
+            </h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <form action="/register" method="POST">
+              <div className="form-group">
+                <label htmlFor="usernameSignup">Username</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="usernameSignup"
+                  placeholder="Enter username"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="passwordSignup">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="passwordSignup"
+                  placeholder="Enter password"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="emailSignup">Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="emailSignup"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email address"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="phoneNumberSignup">Phone Number</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="phoneNumberSignup"
+                  placeholder="Enter phone number"
+                />
+              </div>
+              <div className="modal-footer">
+                <button type="submit" className="btn btn-primary">
+                  Sign Up
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
